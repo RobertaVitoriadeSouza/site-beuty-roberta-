56e186643dc8417ec60b195fa54cfaeba00830e0
Roberta Vitoria <roberta.vaibrilhar@gmail.com>
2025-09-16 01:04:29 -0300

    salvar

--- index.js
+++
@@ -1,9 +0,0 @@
-const slider = new Siema ({
-   loop: true
-})
-
-const buttonPrev = document.querySelector('#prev');
-const buttonNext = document.querySelector('#next');
-
-buttonPrev.addEventListener('click', () => slider.prev());
-buttonNext.addEventListener('click', () => slider.next());
